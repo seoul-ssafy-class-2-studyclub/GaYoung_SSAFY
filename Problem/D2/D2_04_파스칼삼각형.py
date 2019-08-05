47be1a3d71898a740b1a817feeef3426343fe397
GaYoung87 <gyyoon4u@naver.com>
2019-08-05 19:27:27 +0900

    Solve 파스칼 삼각형

--- Problem/D2/D2_04_파스칼삼각형.py
+++ Problem/D2/D2_04_파스칼삼각형.py
@@ -1,12 +1,16 @@
-for t in range(int(input())):
-    N = int(input())
+for t in range(int(input())):  #1
+    N = int(input())    # 4
     result = []
-    for k in range(N):
-        for i in range(N):
-            data = [0] * (i+1)
-            # print(data)
-            data[0] = data[-1] = 1
-            data[k][i+1] += data[k-1][i] + data[k-1][i-1]
-            result.append(data)
 
-print(result)
+    for i in range(N):
+        data = [0] * (i+1)
+        data[0] = data[-1] = 1
+        result.append(data)
+
+    for k in range(2, N):
+        for j in range(1, len(result[k])-1):
+            result[k][j] = result[k-1][j-1] + result[k-1][j]
+    
+    print(f'#{t+1}')
+    for n in result:
+        print(' '.join(map(str, n)))  # join : str만 받음.
