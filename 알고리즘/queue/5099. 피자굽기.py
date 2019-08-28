1bccf12d7cbec868e79e4e2104f4cabc42b0085d
GaYoung87 <gyyoon4u@naver.com>
2019-08-28 23:15:36 +0900

    Solve Queue problem 0828

---
+++ 알고리즘/queue/5099. 피자굽기.py
@@ -0,0 +1,28 @@
+for t in range(int(input())):
+    N, M = map(int, input().split())
+    cheese = []
+    data = list(map(int, input().split()))
+    for m in range(M):
+        cheese.append([m + 1, data[m]])
+
+    queue = []
+    for n in range(N):
+        queue.append(cheese.pop(0))
+
+    result = 0
+    i = 0
+    while queue:   # queue에 값이 들어있을때까지!!!!
+        if queue[i][1] // 2 == 0:
+            if len(cheese) == 0:
+                a = queue.pop(0)
+            elif len(cheese) > 0:
+                queue.pop(0)
+                queue.append(cheese.pop(0))
+        elif queue[i][1] // 2 != 0:
+            if len(queue) == 1:
+                print('break')
+                break
+            else:
+                queue[i][1] = queue[i][1] // 2
+                queue.append(queue.pop(0))
+    print(a[0])
