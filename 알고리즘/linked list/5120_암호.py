4727e83f342b431deccf501bde9c697eff1f30aa
GaYoung87 <gyyoon4u@naver.com>
2019-09-02 18:02:03 +0900

    Solve 7주차 Algo problem 3

--- 알고리즘/linked list/5120_암호.py
+++ 알고리즘/linked list/5120_암호.py
@@ -1,26 +1,26 @@
 for t in range(int(input())):
     N, M, K = map(int, input().split())
-    data = [958, 386, 329, 498, 169, 778]
-    start = 3
-    first = 958
-    # for k in range(K):
-    start = start + M
-    # if start <= len(data) - 2:
-    #     data[start:0] = [data[start - 1] + data[start]]
-    if start == len(data):
-        data[start:0] = [data[start - 1] + first]
-    # if start > len(data) - 1:
-    #     start = start - len(data)
-    #     data[start:0] = [data[start - 1] + data[start]]
-    print(data)
-    # data = data[::-1]
-    #
-    # print('#{}'.format(t + 1), end=' ')
-    # if len(data) > 10:
-    #     for i in range(10):
-    #         print(data[i], end=' ')
-    #     print()
-    # else:
-    #     for i in range(len(data)):
-    #         print(data[i], end=' ')
-    #     print()
+    data = list(map(int, input().split()))
+    start = 0
+    first = data[0]
+    for k in range(K):
+        start = start + M
+        if start <= len(data) - 1:
+            data[start:0] = [data[start - 1] + data[start]]
+        elif start == len(data):
+            data[start:0] = [data[start - 1] + first]
+        elif start > len(data):
+            start = start - len(data)
+            data[start:0] = [data[start - 1] + data[start]]
+
+    data = data[::-1]
+
+    print('#{}'.format(t + 1), end=' ')
+    if len(data) > 10:
+        for i in range(10):
+            print(data[i], end=' ')
+        print()
+    else:
+        for i in range(len(data)):
+            print(data[i], end=' ')
+        print()
