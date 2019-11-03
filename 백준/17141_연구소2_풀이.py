4a78d6e5894644179bea5c513dcf924fcdffca61
GaYoung87 <gyyoon4u@naver.com>
2019-11-03 23:15:42 +0900

    연구소2_미완

--- 백준/17141_연구소2_풀이.py
+++ 백준/17141_연구소2_풀이.py
@@ -19,10 +19,11 @@
 5. 최소시간 구하기
 6. 바이러스를 어떻게 놓아도 모든 빈 칸에 바이러스를 퍼뜨릴 수 없는 경우에는 -1을 출력
 '''
+
 # 바이러스 가능한 경우
 result = []
 def virus(arr, k):
-    if len(arr) == 3:
+    if len(arr) == M:
         result.append(arr)
         return result
     else:
@@ -30,25 +31,26 @@ def virus(arr, k):
             virus(arr + [can_virus[idx]], idx)
 
 
+mymin = 99999999999
 def spread():
+    cnt = 0
     while q:
         x, y = q.pop(0)
         for a, b in near:
             xi, yi = (x + a, y + b)
             if 0 <= xi < N and 0 <= yi < M:
                 if new_board[xi][yi] == 0:
-                    new_board[xi][yi] = 2
+                    new_board[xi][yi] = 1
                     q.append((xi, yi))
-
-
-
-
+                    cnt += 1
+    return cnt
 
 
 N, M = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(N)]
 new_board = [row[:] for row in board]
 near = [(-1, 0), (1, 0), (0, 1), (0, -1)]
+safe = N * N - M
 
 can_virus = []
 # 바이러스 가능한 칸 찾기
@@ -56,31 +58,38 @@ for i in range(N):
     for j in range(N):
         if board[i][j] == 2:
             can_virus.append([i, j])
+            board[i][j] = 0
         elif board[i][j] == 1:
-            new_board[i][j] = -1
+            safe -= 1
 
+'''
+board
+[0, 0, 0, '-', '-']
+[0, '-', '-', '-', '-']
+[0, '-', '-', '-', '-']
+[0, '-', '-', '-', '-']
+[0, 0, 0, '-', '-']
+'''
 
 virus([], -1)
-print(result)
-# [[[0, 0], [1, 5], [4, 3]], [[0, 0], [1, 5], [6, 0]], [[0, 0], [1, 5], [6, 6]], [[0, 0], [4, 3], [6, 0]], [[0, 0], [4, 3], [6, 6]], [[0, 0], [6, 0], [6, 6]], [[1, 5], [4, 3], [6, 0]], [[1, 5], [4, 3], [6, 6]], [[1, 5], [6, 0], [6, 6]], [[4, 3], [6, 0], [6, 6]]]
-
-q = []
+'''
+result
+[[[0, 0], [1, 5], [4, 3]], [[0, 0], [1, 5], [6, 0]], [[0, 0], [1, 5], [6, 6]], [[0, 0], [4, 3], [6, 0]], [[0, 0], [4, 3], [6, 6]], [[0, 0], [6, 0], [6, 6]], [[1, 5], [4, 3], [6, 0]], [[1, 5], [4, 3], [6, 6]], [[1, 5], [6, 0], [6, 6]], [[4, 3], [6, 0], [6, 6]]]
+'''
+cnt = 0
 for r in result:
     q = r
+    new_board = [row[:] for row in board]
     for s in range(M):
         a = r[s][0]
         b = r[s][1]
-        new_board[a][b] = 2  # 바이러스인 곳 표시
-
-
-
-
-
-
-
-
-
-
-
-
-
+        new_board[a][b] = '*'  # 바이러스인 곳 표시
+        for n in new_board:
+            print(n)
+        print(spread())
+        print('====================================')
+# print(board)
+# print(spread(cnt))
+        # print(a)
+        # print('====================================')
+        # break
