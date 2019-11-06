aa9ea6ca4fbc39f921ece3ff4c4de3ba9aea247d
GaYoung87 <gyyoon4u@naver.com>
2019-11-07 02:05:03 +0900

    치킨배달_메모리초과&시간초과

--- 백준/15686_치킨 배달_풀이.py
+++ 백준/15686_치킨 배달_풀이.py
@@ -16,8 +16,6 @@
 2. 0빈칸, 1집, 2치킨집
 3. 치킨집은 치킨집대로, 집은 집대로 모아두고, 선택된 치킨집에서 집까지의 거리 구하기
 '''
-from copy import deepcopy
-
 result = []
 def chicken(arr, k):
     if len(arr) == M:
@@ -30,7 +28,6 @@ def chicken(arr, k):
 
 N, M = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(N)]
-new_board = deepcopy(board)
 
 store = []
 house = []
@@ -42,15 +39,33 @@ for i in range(N):
             house.append([i, j])
 
 chicken([], -1)
-print(result)
 '''
 [result]
 [[[1, 2], [2, 2], [4, 4]], [[1, 2], [4, 4], [4, 4]], [[2, 2], [2, 2], [4, 4]], [[2, 2], [4, 4], [4, 4]], [[4, 4], [2, 2], [4, 4]], [[4, 4], [4, 4], [4, 4]]]
 '''
 
+# 하나의 조합마다 집의 좌표로부터 치킨집거리를 구해서 최솟값 갱신하기
+# 치킨집부터 집구하면 매번 M개만 구할 수 있음
+# 우리가 구해야하는건 여러개의 집마다 치킨집 거리!!
+answer = 99999999
 for r in result:
+    rs = 0  # 조합마다 갱신! (모든 집부터 치킨집 거리를 하나하나 더해서 최소값 찾기)
     for h in house:
         # 집의 좌표
-        a = h[0]
-        b = h[1]
+        # a = h[0]
+        # b = h[1]
+        mymin = 999999  # 최소값으로 갱신 필요, 하나의 조합으로 하나의 집에서 치킨집M개중 고를 수 있음
+        for k in r:
+            # 치킨집 좌표
+            # x = k[0]
+            # y = k[1]
+            # 하나의 집부터 치킨집 거리
+            dis = abs(h[0] - k[0]) + abs(h[1] - k[1])
+            if mymin > dis:
+                mymin = dis
+        # 그러면 모든 집부터 치킨집 거리를 알아야함
+        rs += mymin
+    if answer > rs:
+        answer = rs
 
+print(answer)
