d0d968ea4eaf60ddc7a7311572caeec2f431ae87
GaYoung87 <gyyoon4u@naver.com>
2019-11-15 19:49:04 +0900

    14888_연산자 끼워넣기

--- 백준/AD_시험보충/14888_연산자 끼워넣기.py
+++ 백준/AD_시험보충/14888_연산자 끼워넣기.py
@@ -3,18 +3,39 @@ import itertools
 N = int(input())
 number = list(map(int, input().split()))
 eq = list(map(int, input().split()))
-cal = ['+', '-', '*', '//']
+cal = ['+', '-', '*', '/']
 
-total_cal = []
+eq_t = []
 for i in range(4):
-    total_cal += eq[i] * cal[i]
+    while eq[i] > 0:
+        eq_t.append(cal[i])
+        eq[i] -= 1
 
-result = list(itertools.permutations(total_cal))
+
+result = list(itertools.permutations(eq_t))
 # print(result)  # [('+', '*'), ('*', '+')]
 
-max_value = -1000000000
-min_value = 1000000000
+mymax = -1000000000
+mymin = 1000000000
 
 for r in result:
     total = number[0]
-    for i in range()
+    for i in range(N - 1):
+        calculate = r[i]
+        if calculate == '+':
+            total += number[i + 1]
+        elif calculate == '-':
+            total -= number[i + 1]
+        elif calculate == '*':
+            total *= number[i + 1]
+        elif calculate == '/':
+            total = int(total / number[i + 1])
+
+    if mymax < total:
+        mymax = total
+    if mymin > total:
+        mymin = total
+
+print(mymax)
+print(mymin)
+
