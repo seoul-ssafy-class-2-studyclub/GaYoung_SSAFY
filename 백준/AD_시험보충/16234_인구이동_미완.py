2a9c162e4c584c33d454525fbdfc52d919e1b653
GaYoung87 <gyyoon4u@naver.com>
2019-11-18 19:30:19 +0900

    인구이동_수정

--- 백준/AD_시험보충/16234_인구이동_미완.py
+++ 백준/AD_시험보충/16234_인구이동_미완.py
@@ -1,39 +1,54 @@
 from collections import deque
-from pprint import pprint
 
 def bfs():
-    while q:
-        x, y = q.popleft()
-        for a, b in near:
-            xi, yi = x + a, y + b
-            if 0 <= xi < N and 0 <= yi < N and visit[xi][yi] == False:
-                if L <= abs(board[xi][yi] - board[x][y]) <= R:
-                    visit[xi][yi] = True
-                    ls.append((xi, yi, board[xi][yi]))
-                    q.append((xi, yi))
+    pass
 
 
 
 N, L, R = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(N)]
 
-
 near = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-
-visit = [[False] * N for _ in range(N)]
-
-for i in range(N):
-    for j in range(N):
-        ls = deque()
-        q = deque()
-        if board[i][j] != 0 and visit[i][j] == False:
-            q.append((i, j))
-            ls.append((i, j, board[i][j]))
-            visit[i][j] = True
-            bfs()
+cnt = 0
 
 
-            # pprint(visit)
-            # print(ls)
+total = [0]
+total_num = []
 
-            if len(ls) != 0:
+while total:
+    total = []
+    total_num = []
+    visit = [[False] * N for _ in range(N)]
+    for i in range(N):
+        for j in range(N):
+            q = deque()
+            if board[i][j] != 0 and not visit[i][j]:
+                ls = [(i, j)]
+                ls_total = [board[i][j]]
+                q.append((i, j))
+                visit[i][j] = True
+                while q:
+                    x, y = q.popleft()
+                    for a, b in near:
+                        xi, yi = x + a, y + b
+                        if 0 <= xi < N and 0 <= yi < N and not visit[xi][yi]:
+                            if L <= abs(board[xi][yi] - board[x][y]) <= R:
+                                q.append((xi, yi))
+                                ls.append((xi, yi))
+                                ls_total.append(board[xi][yi])
+                                visit[xi][yi] = True
+
+                if len(ls) != 1:
+                    total.append(ls)
+                    total_num.append(ls_total)
+
+    if len(total_num) == 0:
+        break
+    else:
+        for i in range(len(total_num)):
+            mymean = total_num[i] // total[i]
+            for y, x in total[i]:
+                board[y][x] = mymean
+
+    cnt += 1
+print(cnt)
--- 백준/AD_시험보충/2636_치즈_미완.py
+++ 백준/AD_시험보충/2636_치즈_미완.py
@@ -1,90 +1,45 @@
-import collections
 from pprint import pprint
+from collections import deque
 
 near = [(-1, 0), (0, 1), (1, 0), (0, -1)]
-def out_air(x, y):
+def out_air(i, j):
     for a, b in near:
-        if 0 <= x + a < N and 0 <= y + b < M and board[x + a][y + b] == 0:
-            board[x + a][y + b] = 2
-            out_air(x + a, y + b)
+        ii, jj = i + a, j + b
+        if 0 <= ii < N and 0 <= jj < M and board[ii][jj] == 0:
+            board[ii][jj] = 2
+            out_air(ii, jj)
 
 
 def melt():
     for i in range(N):
         for j in range(M):
             for a, b in near:
-                if 0 <= i + a < N and 0 <= j + b < M and board[i][j] == 1:
-                    if board[i + a][j + b] == 2:
-                        ls.append((i + a, j + b))
+                xi, yi = i + a, j + b
+                if 0 <= xi < N and 0 <= yi < M:
+                    if board[xi][yi] == 2:
+                        board[xi][yi] = 3
 
-    for l in range(len(ls)):
-        x, y = ls.pop(0)
-        board[x][y] = 2
-    return 1
-
-
-def count_c():
-    cnt = 0
     for i in range(N):
         for j in range(M):
-            if board[i][j] == 1:
-                cnt += 1
-    return cnt
+            if board[i][j] == 3:
+                board[i][j] = 2
+
+
 
 
 N, M = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(N)]
-q = collections.deque()
 
 board[0][0] = 2
 out_air(0, 0)
-
 # pprint(board)
-'''
-[[2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
- [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2],
- [2, 2, 2, 2, 2, 2, 2, 1, 1, 2, 2, 2],
- [2, 1, 1, 1, 2, 2, 2, 1, 1, 2, 2, 2],
- [2, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2],
- [2, 1, 1, 1, 1, 1, 0, 1, 1, 2, 2, 2],
- [2, 1, 1, 1, 1, 0, 0, 1, 1, 2, 2, 2],
- [2, 2, 1, 1, 0, 0, 0, 1, 1, 2, 2, 2],
- [2, 2, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2],
- [2, 2, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2],
- [2, 2, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2],
- [2, 2, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2],
- [2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2]]
-'''
 
-cheeze = 1  # 치즈 갯수
-queue = collections.deque()
-ls = []
-hour = 0
-last = 0
-# while cheeze != 0:
-cheeze = count_c()
+        #
+        #
+        # if board[i][j] == 1:
+        #
+        #     q.append((i, j))
+        #     melt()
 
-# if cheeze != 0:
-#     last = cheeze
-# else:
-#     break
-#
-
-hour += melt()
-
-for i in range(N):
-    for j in range(M):
-        if board[i][j] == 0:
-            for a, b in near:
-                ii, jj = i + a, j + b
-                if 0 <= ii < N and 0 <= jj < M and board[ii][jj] == 2:
-                    # 외부공기 시작 칸을 2로 바꾸고 시작
-                    board[ii][jj] = 2
-                    out_air(ii, jj)
-
-
-print(ls)
-pprint(board)
-print(hour)
-print(last)
-
+            # pprint(board)
+            # print('====================================')
