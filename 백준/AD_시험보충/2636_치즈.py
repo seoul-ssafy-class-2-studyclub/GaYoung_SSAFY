fdbca6d40c30709baa84c619da2dfc1da4b66e9b
GaYoung87 <gyyoon4u@naver.com>
2019-11-19 08:52:28 +0900

    치즈

--- 백준/AD_시험보충/2636_치즈.py
+++ 백준/AD_시험보충/2636_치즈.py
@@ -1,6 +1,3 @@
-from pprint import pprint
-from collections import deque
-
 near = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 def out_air(i, j):
     for a, b in near:
@@ -13,10 +10,9 @@ def out_air(i, j):
 def melt():
     for i in range(N):
         for j in range(M):
-            if board[i][j] == 1:
                 for a, b in near:
                     xi, yi = i + a, j + b
-                    if 0 <= xi < N and 0 <= yi < M:
+                    if 0 <= xi < N and 0 <= yi < M and board[xi][yi] == 1:
                         if board[xi][yi] == 2:
                             board[xi][yi] = 3
 
@@ -25,21 +21,45 @@ def melt():
             if board[i][j] == 3:
                 board[i][j] = 2
 
+    return 1
 
+def count_cheeze():
+    cnt = 0
+    for i in range(N):
+        for j in range(M):
+            if board[i][j] == 1:
+                cnt += 1
+    return cnt
 
 
 N, M = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(N)]
+cheeze = N * M
 
 board[0][0] = 2
 out_air(0, 0)
-# pprint(board)
 
-for i in range(N):
-    for j in range(M):
-        if board[i][j] == 0:
-            for a, b in near:
-                ii, jj = i + a, j + b
-                if 0 <= ii < N and 0 <= jj < M and board[ii][jj] == 2:
-                    board[i][j] = 2
-                    out_air(i, j)
+hour = 0
+rs = 0
+while cheeze != 0:
+
+    cheeze = count_cheeze()
+
+    if cheeze:
+        rs = cheeze
+    else:
+        break
+
+    for i in range(N):
+        for j in range(M):
+            if board[i][j] == 0:
+                for a, b in near:
+                    ii, jj = i + a, j + b
+                    if 0 <= ii < N and 0 <= jj < M and board[ii][jj] == 2:
+                        board[i][j] = 2
+                        out_air(i, j)
+
+    hour += melt()
+
+print(hour)
+print(rs)
