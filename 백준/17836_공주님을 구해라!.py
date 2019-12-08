4edfeba755a42014c0a22554888b6dd8db97a57f
GaYoung87 <gyyoon4u@naver.com>
2019-12-08 13:53:41 +0900

    공주님을 구해라

--- 백준/17836_공주님을 구해라!.py
+++ 백준/17836_공주님을 구해라!.py
@@ -7,6 +7,7 @@
 0 1 1 1 1 1
 0 0 0 0 0 0
 '''
+
 from collections import deque
 from pprint import pprint
 N, M, T = map(int, input().split())
@@ -16,23 +17,74 @@ near = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 q = [(0, 0)]
 board[0][0] = 1
 time = 0
-print(q)
-while q:
-    if time > T:
-        break
 
+time_gram = 0
+result = 9999999999
+gram = False
+flag = False
+while q:
     time += 1
+    # print(time, T)
+    if time > T:
+        # print('-------------------')
+        break
+        # flag ==False -> fail
+
     for i in range(len(q)):
         x, y = q.pop(0)
         for a, b in near:
             xi, yi = x + a, y + b
             if 0 <= xi < N and 0 <= yi < M:
-                if board[xi][yi] == 0:
+                if xi == N - 1 and yi == M - 1:
+                    result = time
+                    # 공주 만나면 stop
+                    flag = True
+                    break
+
+                elif board[xi][yi] == 0:
+                    # visit을 1로 표현
                     board[xi][yi] = 1
                     q.append((xi, yi))
-                elif board[xi][yi] == 2:
+
+                    # pprint(board)
+                    # print(time)
+                    # print(flag)
+                    # print('===========================')
+
+                elif gram == False and board[xi][yi] == 2:
                     if time + abs(xi - N + 1) + abs(yi - M + 1) <= T:
-                        time = time + abs(xi - N + 1) + abs(yi - M + 1)
+                        time_gram = time + abs(xi - N + 1) + abs(yi - M + 1)
+                        gram = True
+                        # print(time)   # 10
+        if flag:
+            break
+
+    if flag:
+        break
 
 
-print(time)
+if flag == False and gram == False:
+    print('Fail')
+elif flag and gram:
+    if time_gram != 0 and result >= time_gram:
+        result = time_gram
+    print(result)
+elif flag == False and gram == True:
+    print(time_gram)
+elif flag and gram == False:
+    print(result)
+
+'''
+5 5 100
+0 0 0 0 0
+0 0 1 1 1
+0 0 1 0 2
+0 0 1 1 1
+0 0 0 1 0
+'''
+
+'''
+2 2 1
+0 2
+0 0
+'''
