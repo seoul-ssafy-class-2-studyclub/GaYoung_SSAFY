0eacd1b85ad67c71912ddaf22835c4281b1b24c2
GaYoung87 <gyyoon4u@naver.com>
2020-02-11 01:06:21 +0900

    미세먼지 ing

--- Baekjoon/17144_미세먼지 안녕!.py
+++ Baekjoon/17144_미세먼지 안녕!.py
@@ -1,3 +1,32 @@
+near = [(-1, 0), (1, 0), (0, 1), (0, -1)]
+def spread():
+    bd = [row[:] for row in board]
+
+    for i in range(N):
+        for j in range(M):
+            if board[i][j] > 0:
+                temp = board[i][j] // 5
+                for a, b in near:
+                    ii, jj = i + a, j + b
+                    if 0 <= ii < N and 0 <= jj < M:
+                        if board[ii][jj] != -1:
+                            bd[ii][jj] += temp
+                            bd[i][j] -= temp
+
+    # board를 바뀐 bd와 같게 만들기
+    for i in range(N):
+        for j in range(M):
+            board[i][j] = bd[i][j]
+
+    return bd
+
+
+def rotate():
+
+
 N, M, T = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(N)]
 
+for t in range(T):
+    spread()
+    rotate()
