ea8612de2369772da590907a5ef91b0f8d0ce115
GaYoung87 <gyyoon4u@naver.com>
2020-03-02 14:19:08 +0900

    숫자만들기

--- Problem/모의 SW/4008_숫자 만들기/4008_숫자만들기.py
+++ Problem/모의 SW/4008_숫자 만들기/4008_숫자만들기.py
@@ -1,7 +1,52 @@
+from collections import deque
 
+def permutation(arr, k):
+    global q
 
-N = int(input())
-operator = list(map(int, input().split()))
-oper = [1, 2, 3, 4]
-numbers = list(map(int, input().split()))
+    if k == N - 1:
+        q.append(arr)
+        return
 
+    else:
+        for i in range(4):
+            if operations[i] > 0:
+                operations[i] -= 1
+                permutation(arr + [i], k + 1)
+                operations[i] += 1
+    return
+
+for t in range(int(input())):
+    N = int(input())
+
+    # ['+', '-', '*', '//']
+    operations = list(map(int, input().split()))
+    numbers = list(map(int, input().split()))
+
+    q = deque()
+    permutation([], 0)
+
+    max_result = -100000001
+    min_result = 100000001
+
+    for operation in q:
+        result = numbers[0]
+        for i in range(N - 1):
+            if operation[i] == 0:
+                result += numbers[i + 1]
+
+            elif operation[i] == 1:
+                result -= numbers[i + 1]
+
+            elif operation[i] == 2:
+                result *= numbers[i + 1]
+
+            else:
+                result = int(result / numbers[i + 1])
+
+        if result > max_result:
+            max_result = result
+
+        if result < min_result:
+            min_result = result
+
+    print('#{} {}'.format(t + 1, max_result - min_result))
