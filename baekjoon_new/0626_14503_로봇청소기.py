7dbbfe17b03823e5636ce8dc35c73b597b4e8320
GaYoung87 <gyyoon4u@naver.com>
2020-06-27 16:05:47 +0900

    0627

--- baekjoon_new/0626_14503_로봇청소기.py
+++ baekjoon_new/0626_14503_로봇청소기.py
@@ -2,37 +2,14 @@ N, M = map(int, input().split())
 r, c, d = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(N)]
 
-dx = [-1, 0, 1, 0]
-dy = [0, 1, 0, -1]
+'''
+1. 현재 위치를 청소한다.
+2. 현재 위치에서 현재 방향을 기준으로 왼쪽방향부터 차례대로 탐색을 진행한다.
+  2-1. 왼쪽 방향에 아직 청소하지 않은 공간이 존재한다면, 그 방향으로 회전한 다음 한 칸을 전진하고 1번부터 진행한다.
+  2-2. 왼쪽 방향에 청소할 공간이 없다면, 그 방향으로 회전하고 2번으로 돌아간다.
+  2-3. 네 방향 모두 청소가 이미 되어있거나 벽인 경우에는, 바라보는 방향을 유지한 채로 한 칸 후진을 하고 2번으로 돌아간다.
+  2-4. 네 방향 모두 청소가 이미 되어있거나 벽이면서, 뒤쪽 방향이 벽이라 후진도 할 수 없는 경우에는 작동을 멈춘다.
+'''
+near = [(-1, 0), (0, 1), (1, 0), (0, -1)]
 cnt = 0
-x, y, d = r, c, d
-stop = False
-flag = True
-while True:
 
-    if stop:
-        break
-    if flag:
-        board[x][y] = 2
-        cnt += 1
-    flag = False
-    for i in range(4):
-        dd = (d + 3 - i) % 4
-        xi, yi = x + dx[dd], y + dy[dd]
-        if board[xi][yi] == 0:
-            flag = True
-            break
-
-    if flag:  # 왼쪽확인 경우
-        x, y, d = xi, yi, dd
-        continue
-    else:  # flag == False
-
-        xx, yy = x - dx[d], y - dy[d]
-        if board[xx][yy] == 1:  # d
-            stop = True
-
-        else:
-            x, y, d = xx, yy, d
-
-print(cnt)
---
+++ programmers/0627_크레인 인형뽑기 게임.py
@@ -0,0 +1,44 @@
+def solution(board, moves):
+    answer = 0
+
+    B = len(board)
+    bd = [[0 for _ in range(B)] for _ in range(B)]
+    for i in range(B):
+        for j in range(B):
+            bd[i][j] = board[j][i]
+
+    result = [0]
+    answer = 0
+    for m in moves:
+        cnt = 0  # bd[m-1]이 다 0으로 되어있는지 확인하기 위해서
+        for k in range(len(bd[m - 1])):
+            if bd[m - 1][k] != 0:  # for k in bd[m-1]:해서 if k !=0 이라고하면 이따 k=0으로 하면 board에 안바뀜
+                if result[-1] == bd[m - 1][k]:
+                    result.pop()
+                    answer += 2
+                else:
+                    result.append(bd[m - 1][k])
+                bd[m - 1][k] = 0
+                break
+
+            cnt += 1
+
+            if cnt == len(bd[m - 1]):
+                break
+
+    return answer
+
+board = [[0,0,0,0,0],[0,0,1,0,3],[0,2,5,0,1],[4,2,4,4,2],[3,5,1,3,1]]
+moves = [1,5,3,5,1,2,1,4]
+
+'''
+1. board가 가로로 맨 윗줄부터 나와있는것이기 때문에 세로로 돌린다 
+ board = [[0,0,0,4,3],
+          [0,0,2,2,5],
+          [0,1,5,4,1],
+          [0,0,0,4,3],
+          [0,3,1,2,1]]
+2. for i in moves: moves 돌면서 해당 board[i-1]중에 0이 아닌 첫번째값이 나오면 
+   answer리스트에 추가하고 0으로 바꾼다
+     - 이때, board판에 다 0이면 아무것도 일어나지 않음
+'''
