7748d69728ea3850bd999d4120e1fc4273388ceb
GaYoung87 <gyyoon4u@naver.com>
2020-08-03 15:28:32 +0900

    다리를 지나는 트럭 완성

--- programmers/0803_다리를 지나는 트럭.py
+++ programmers/0803_다리를 지나는 트럭.py
@@ -1,31 +1,25 @@
+from collections import deque
+
 def solution(bridge_length, weight, truck_weights):
     answer = 0
-    return answer
-
-
-bridge_length=2
-weight=10
-truck_weights=[7,4,5,6]
-
-answer = 0
-check = [0] * bridge_length
-
-while truck_weights:
-    answer += 1
-    x = check.pop(0)
-    if truck_weights[0] + x + sum(check) > weight:
+    check = deque()
+    for j in range(bridge_length):
         check.append(0)
-    else:
-        y = truck_weights.pop(0)
-        check.append(y)
-
-    # print(check)
-
-    while len(check) == 0:
+    trucks = deque()
+    for i in range(len(truck_weights)):
+        trucks.append(truck_weights[i])
+    now_weight = 0
+    while trucks:
         answer += 1
-        check.pop(0)
-
-    # print(answer)
-
-
-
+        x = check.popleft()  # 나갈것 내보내고
+        now_weight -= x
+        if trucks[0] + now_weight > weight:  # 못 들어가면
+            check.append(0)
+        else:  # 들어가면
+            y = trucks.popleft()
+            check.append(y)
+            now_weight += y
+    while len(check) != 0:
+        answer += 1
+        check.popleft()
+    return answer
