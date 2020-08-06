f9169f7bd9684b4dc201b87fdaac5f473d447f70
GaYoung87 <gyyoon4u@naver.com>
2020-08-07 00:47:25 +0900

    스킬트리

--- programmers/0806_스킬트리.py
+++ programmers/0806_스킬트리.py
@@ -1,7 +1,30 @@
 def solution(skill, skill_trees):
-    answer = 0
-    return answer
+    cnt = 0
+    for sk in skill_trees:
+        res = ''
+        for s in sk:
+            if s in skill:
+                res += s
+
+        if res == skill:
+            cnt += 1
+        elif len(res) == 0:
+            cnt += 1
+        # 'BC'
+        elif 0 < len(res) <= len(skill):
+            temp = 0
+            for r in range(len(res)):
+                if res[r] == skill[r]:
+                    temp += 1
+
+            if temp == len(res):
+                cnt += 1
+
+    # print(cnt)
+
+    return cnt
 
 
 skill = 'CBD'
 skill_trees = ["BACDE", "CBADF", "AECB", "BDA"]
+
