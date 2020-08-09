2a74c1282c09a56882af9a024cb31191e229dbe4
GaYoung87 <gyyoon4u@naver.com>
2020-08-09 22:21:16 +0900

    0809

---
+++ programmers/0809_문자열 압축.py
@@ -0,0 +1,32 @@
+def solution(skill, skill_trees):
+    cnt = 0
+    for sk in skill_trees:
+        res = ''
+        for s in sk:
+            if s in skill:
+                res += s
+
+        if res == skill:
+            cnt += 1
+        elif len(res) == 0:
+            cnt += 1
+        # 'BC'
+        # 'CBD', 'CB' , 'BC'
+        elif 0 < len(res) <= len(skill):
+            temp = 0
+            for r in range(len(res)):
+                if res[r] == skill[r]:
+                    temp += 1
+
+            if temp == len(res):
+                cnt += 1
+
+    # print(cnt)
+
+    return cnt
+
+
+skill = 'CBD'
+skill_trees = ["BACDE", "CBADF", "AECB", "BDA"]
+['BCD', 'CBD', 'BD']
+
