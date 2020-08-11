1b9aff515c3e66f6bd244dfa32d4977080d4a4ac
GaYoung87 <gyyoon4u@naver.com>
2020-08-11 20:20:21 +0900

    0811

--- programmers/0811_카카오프렌즈 컬러링북.py
+++ programmers/0811_카카오프렌즈 컬러링북.py
@@ -1,8 +1,43 @@
 from collections import deque
+from copy import deepcopy
 
 def solution(m, n, picture):
-    answer = []
-    return answer
+    near = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    # visit = [[0 for _ in range(n)] for _ in range(m)]
+    pic = deepcopy(picture)
+    # print(visit)
+    mymax = 0
+    q = deque()
+    cnt = 0
+    result = []
+    for i in range(m):
+        for j in range(n):
+            if picture[i][j] != 0:
+                q.append([i, j])
+                cnt += 1
+
+                answer = 0
+                while q:
+                    x, y = q.popleft()
+                    temp = pic[x][y]
+                    # print(temp)
+
+                    for a, b in near:
+                        xi, yi = x + a, y + b
+                        if 0 <= xi < m and 0 <= yi < n and picture[xi][yi] == temp:
+                            picture[xi][yi] = 0
+                            q.append([xi, yi])
+                            answer += 1
+                    # print(q)
+
+                if mymax < answer:
+                    mymax = answer
+
+    result.append(cnt)
+    result.append(mymax)
+    # print(result)
+
+    return result
 
 
 m, n = 6, 4
@@ -14,29 +49,3 @@ picture = [
     [0, 0, 0, 3],
     [0, 0, 0, 3]]
 
-near = [(0, 1),(1, 0),(0, -1),(-1, 0)]
-visit = [[0 for _ in range(n)] for _ in range(m)]
-print(visit)
-total = 0
-
-q = deque()
-for i in range(m):
-    for j in range(n):
-        if picture[i][j] != 0:
-            q.append([i, j])
-
-def bfs():
-    answer = 0
-    while q:
-        x, y = q.popleft()
-        temp = picture[x][y]
-
-        for a, b in near:
-            xi, yi = x + a, y + b
-            if 0 <= xi < m and 0 <= yi < n and visit[xi][yi] == 0:
-                pass
-
-
-
-
-    return answer
