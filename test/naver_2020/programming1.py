37cc88406b8eb63643cfbe2cc99c097614501041
GaYoung87 <gyyoon4u@naver.com>
2020-09-28 02:06:24 +0900

    0928

--- test/naver_2020/programming1.py
+++ test/naver_2020/programming1.py
@@ -1,12 +1,25 @@
-m = "kkaxbycyz"
-k = "abc"
+# m = "kkaxbycyz"
+# k = "abc"
 
 # m = "acbbcdc"
 # k = "abc"
 
-# m = "aabcb"
-# k = "ab"
+m = "aabcb"
+k = "ab"
 
 answer = ''
-k_dict = enumerate(k, 1)
-print(k_dict)
+m_idx = -1
+k_idx = 0
+while True:
+    m_idx += 1
+
+    if m[m_idx] == k[k_idx]:
+        k_idx += 1
+        if k_idx == len(k):
+            answer += m[m_idx+1:]
+            break
+
+    elif m[m_idx] != k[k_idx]:
+        answer += m[m_idx]
+
+print(answer)
