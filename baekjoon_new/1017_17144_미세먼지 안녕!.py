2ec54890a7a77ba41767cffe795785a5e82807ed
GaYoung87 <gyyoon4u@naver.com>
2020-10-18 00:06:54 +0900

    1017

--- baekjoon_new/1017_17144_미세먼지 안녕!.py
+++ baekjoon_new/1017_17144_미세먼지 안녕!.py
@@ -34,19 +34,36 @@ def munji():
 
 def clean():
     # up
-    state = board[up[0]][C - 1]
-    # 오른쪽
-
-    # 위쪽
-    # 왼쪽
+    # 오른쪽 - 위쪽 - 왼쪽 - 아래쪽 반대 순으로 진행!!!!
     # 아래쪽
+    for i in range(up[0] - 1, 0, -1):
+        board[i][0] = board[i - 1][0]
+    # 왼쪽
+    for i in range(C - 1):
+        board[0][i] = board[0][i + 1]
+    # 위
+    for i in range(up[0]):
+        board[i][C - 1] = board[i + 1][C - 1]
+    # 오른쪽
+    for i in range(C - 1, 0, -1):
+        board[up[0]][i] = board[up[0]][i - 1]
+    board[up[0]][1] = 0  # 공기청정기에서 나온바람으로 바로 옆칸은 0이 된다.
 
     # down
-    # 오른쪽
-    # 아래쪽
-    # 왼쪽
+    # 오른쪽 - 아래쪽 - 왼쪽 - 위쪽 반대 순으로 진행!!!!
     # 위쪽
-
+    for i in range(down[0] + 1, R - 1):
+        board[i][0] = board[i + 1][0]
+    # 왼쪽
+    for i in range(C - 1):
+        board[R - 1][i] = board[R - 1][i + 1]
+    # 아래쪽
+    for i in range(R - 1, down[0], -1):
+        board[i][C - 1] = board[i - 1][C - 1]
+    # 오른쪽
+    for i in range(C - 1, 1, -1):
+        board[down[0]][i] = board[down[0]][i - 1]
+    board[down[0]][1] = 0
 
 R, C, T = map(int, input().split())
 board = [list(map(int, input().split())) for _ in range(R)]
@@ -54,5 +71,12 @@ board = [list(map(int, input().split())) for _ in range(R)]
 up, down = robot()
 for _ in range(T):
     munji()
-    # clean()
-    print(board)
+    clean()
+
+answer = 0
+for i in range(R):
+    for j in range(C):
+        if board[i][j] > 0:
+            answer += board[i][j]
+
+print(answer)
