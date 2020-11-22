0a1ecc16a10c8131c471328cd5923ed709147dfa
Gayoung <gyyoon4u@naver.com>
2020-11-22 22:29:02 +0900

    1122

---
+++ programmers/1123_수식 최대화.py
@@ -0,0 +1,11 @@
+def solution(n, weak, dist):
+    answer = 0
+    return answer
+
+n = 12
+weak = [1, 5, 6, 10]
+dist = [1, 2, 3, 4]	 # 2
+
+# n = 12
+# weak = [1, 3, 4, 9, 10]
+# dist = [3, 5, 7]  # 1
